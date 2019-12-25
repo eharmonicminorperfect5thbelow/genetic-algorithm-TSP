b07838dd57a057ad42a385437a97c42162cf61b6
Akira Tetsuka <eharmonicminorperfect5thbelow@gmail.com>
2019-12-25 12:05:39 +0900

    update

--- GA.hpp
+++ GA.hpp
@@ -9,70 +9,70 @@ using namespace std;
 class GA
 {
 public:
-	// 都市
-	// x座標, y座標
-	struct City
-	{
-		double x;
-		double y;
+    // 都市
+    // x座標, y座標
+    struct City
+    {
+        double x;
+        double y;
 
-		City(double x, double y) : x(x), y(y) {}
-	};
+        City(double x, double y) : x(x), y(y) {}
+    };
 
-	// 染色体(chromosome)
-	// 遺伝子(genes)とそれに対応する適合度(fitness)
-	struct Chromosome
-	{
-		vector<int> genes;
-		double fitness;
-	};
+    // 染色体(chromosome)
+    // 遺伝子(genes)とそれに対応する適合度(fitness)
+    struct Chromosome
+    {
+        vector<int> genes;
+        double fitness;
+    };
 
-	// コンストラクタ
-	GA(const char* instance, int populationSize, int generationSize, float selectivePressure, float crossoverRatio, float crossoverProbability, float mutateProbability);
-	
-	// 計算を実行
-	void solve();
+    // コンストラクタ
+    GA(const char* instance, int populationSize, int generationSize, float selectivePressure, float crossoverRatio, float crossoverProbability, float mutateProbability);
+    
+    // 計算を実行
+    void solve();
 
 private:
-	// 都市間の距離を計算
-	double dist(const City& a, const City& b)
-	{
-		return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
-	}
+    // 都市間の距離を計算
+    double dist(const City& a, const City& b)
+    {
+        return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+    }
 
-	// インスタンスの都市の配列
-	vector<City> cities;
-	// 集団
-	vector<Chromosome> population;
-	// 選択(select)により得られた集団
-	vector<Chromosome> matingPool;
-	// その時点で最も適合度(fitness)の良い個体
-	Chromosome best;
-	// 最大の世代数
-	int generationSize;
-	// 集団内の個体数
-	int populationSize;
-	// 染色体の長さ
-	int chromosomeSize;
-	// 集団内での適合度の順位に対応した選択確率
-	vector<float> selectProbabilities;
-	// 交叉(crossover)でedge recombinationを用いる割合
-	float crossoverRatio;
-	// 交叉を行う確率
-	float crossoverProbability;
-	// 突然変異を行う確率
-	float mutateProbability;
+    // インスタンスの都市の配列
+    vector<City> cities;
+    // 集団
+    vector<Chromosome> population;
+    // 選択(select)により得られた集団
+    vector<Chromosome> matingPool;
+    // その時点で最も適合度(fitness)の良い個体
+    Chromosome best;
+    // 最大の世代数
+    int generationSize;
+    // 集団内の個体数
+    int populationSize;
+    // 染色体の長さ
+    int chromosomeSize;
+    // 集団内での適合度の順位に対応した選択確率
+    vector<float> selectProbabilities;
+    // 交叉(crossover)でedge recombinationを用いる割合
+    float crossoverRatio;
+    // 交叉を行う確率
+    float crossoverProbability;
+    // 突然変異を行う確率
+    float mutateProbability;
 
-	// 初期集団の生成
-	void initialize();
-	// 適合度の計算
-	void evaluate();
-	// 選択
-	void select();
-	// 交叉
-	void crossover();
-	// 突然変異
-	void mutate();
+    // 初期集団の生成
+    void initialize();
+    // 適合度の計算
+    void evaluate();
+    // 選択
+    void select();
+    // 交叉
+    void crossover();
+    // 突然変異
+    void mutate();
 };
 
 #endif
--- example.cpp
+++ example.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-	GA ga("bays29.txt", 100, 2000, 2.0, 0.5, 0.8, 0.05);
-	ga.solve();
+    GA ga("bays29.txt", 100, 2000, 2.0, 0.5, 0.8, 0.05);
+    ga.solve();
 
-	return 0;
+    return 0;
 }
